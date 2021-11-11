f3d811da56fe51e1755f963cea96b06bdbad319d
Chris Paul <cpaul0013@gmail.com>
2021-11-11 16:49:01 -0500

    My Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Menu } from './menu';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Menu';
+menuList:Menu[] = [
+  {name:"Chicken Fingers", category: "Dinner", price: 11.99},
+  {name:"Pizza", category: "Dinner", price: 11.99},
+  {name:"Wings", category: "Sides", price: 8.99},
+  {name:"Breadsticks", category: "Sides", price: 4.99},
+  {name:"Casesar Salad", category: "Salads", price: 5.99},
+  {name:"Cinnamon Roll", category: "Dessert", price: 8.99}
+];
 }
+  
+
